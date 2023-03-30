a503be4c87f4fd031a6e40835f964d681fc7fcca
Gerbil789 <67593297+Gerbil789@users.noreply.github.com>
2023-03-30 12:15:00 +0200

    WPF (CV5)

---
+++ CustomerWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for CustomerWindow.xaml
+    /// </summary>
+    /// 
+    public delegate void SaveCustomer(Customer c);
+    public partial class CustomerWindow : Window
+    {
+        public Customer Customer { get; set; } = new Customer();
+        public event SaveCustomer OnSave;
+        public CustomerWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this.Customer;
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+            this.OnSave?.Invoke(this.Customer);
+            this.Close();
+        }
+    }
+}
